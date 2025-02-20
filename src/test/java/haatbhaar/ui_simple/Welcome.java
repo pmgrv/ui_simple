da4feacf647fe7e872a7431fdfeaab35e15b921c
Pravinkumar Raut <raut.pravinkumar116@gmail.com>
2025-02-20 23:24:58 +0530

    Log4j integrated successfully!!!

--- src/test/java/haatbhaar/ui_simple/Welcome.java
+++ src/test/java/haatbhaar/ui_simple/Welcome.java
@@ -1,18 +1,15 @@
 package haatbhaar.ui_simple;
-import java.util.logging.*;
+import org.testng.annotations.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.Test;
 
 public class Welcome {
-	private Logger log = Logger.getLogger(Welcome.class.getName());
 	@Test
 	public void StartTest() {
 		System.setProperty("webdriver.chrome.driver", "src/main/utils/chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://google.com");
 		System.out.println(driver.getTitle());
-		log.info("First Success Test Case");
 		driver.quit();
 	}
 }
